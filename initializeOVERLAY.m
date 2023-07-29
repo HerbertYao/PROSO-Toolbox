35048de193c6bba6d82925f5bb234f96d5470cc9
HerbertYao <62777583+HerbertYao@users.noreply.github.com>
2023-07-29 16:38:00 -0400

    Create initializeOVERLAY.m

---
+++ initializeOVERLAY.m
@@ -0,0 +1,34 @@
+function initializeOVERLAY()
+
+% Initialization and set up
+
+fprintf('============================\n');
+fprintf('   OVERLAY Toolbox v1.0.1   \n');
+fprintf('============================\n');
+
+% Check availability of COBRA Toolbox
+
+if isempty(which('optimizeCbModel.m'))
+    try
+        warning('Boosting COBRA Toolbox\n');
+        initCobraToolbox(false);
+    catch
+        error('COBRA Toolbox not installed or not in path');
+    end
+end
+
+% Add src to path and check the integrity of the file structure
+lastwarn('','');
+pth = erase(which('initializeOVERLAY.m'),'initializeOVERLAY.m');
+
+addpath(pth);
+addpath([pth,'src']);
+addpath([pth,'src/MOPA']);
+addpath([pth,'src/OVERLAY']);
+addpath([pth,'src/PC-OptKnock']);
+
+if ~isempty(lastwarn())
+    error('Incomplete OVERLAY Toolbox');
+end
+
+end
